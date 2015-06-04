6c6e410751b57fdf996ec55c23ee2a33a63b0038
Sean McLellan <sean.mclellan@sixconcepts.com>
2015-06-04 10:48:23 -0400

    Misc nancy stuffs.

---
+++ BaristaLabsHost/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace BaristaLabsHost
+{
+    using Nancy.Hosting.Self;
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new NancyHost(new Uri("http://localhost:1234")))
+            {
+                host.Start();
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Framework/BaristaLabs.Espresso.Core/v1/EspressoEngineApplicationStartup.cs
@@ -0,0 +1,24 @@
+﻿namespace BaristaLabs.Espresso.Core.v1
+{
+    using Nancy.Bootstrapper;
+    using System;
+
+    public class EspressoEngineApplicationStartup : IApplicationStartup
+    {
+        //private IFooFactory fooFactory;
+        //public EspressoEngineApplicationStartup(IFooFactory fooFactory)
+        //{
+        //    this.fooFactory = fooFactory;
+        //}
+
+        public void Initialize(IPipelines pipelines)
+        {
+            pipelines.BeforeRequest.AddItemToStartOfPipeline(ctx => {
+                //var foo = fooFactory.Make();
+                //foo.Bar(ctx);
+                Console.WriteLine("Hi");
+                return null;
+            });
+        }
+    }
+}
